983d9de13e5686c26c0e9653e77618028d14e645
Erlangga Fajar Ramadhan <163155518+erfjrmd@users.noreply.github.com>
2024-11-21 21:03:08 +0700

    Update disable.js

--- js/disable.js
+++ js/disable.js
@@ -1,8 +1,18 @@
+// Disable right-click (context menu)
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 // Disable F12 and Ctrl+Shift+I (DevTools)
 document.onkeydown = function (e) {
-    if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && e.keyCode == 73)) {
+    // Disable F12
+    if (e.keyCode == 123) {
+        return false;
+    }
+    // Disable Ctrl+Shift+I (DevTools)
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        return false;
+    }
+    // Disable Ctrl+U (View Source)
+    if (e.ctrlKey && e.keyCode == 85) {
         return false;
     }
 };
@@ -10,4 +20,4 @@ document.onkeydown = function (e) {
 // Make the page visible after everything is loaded
 window.onload = function () {
     document.documentElement.style.visibility = 'visible';
-}
+};
